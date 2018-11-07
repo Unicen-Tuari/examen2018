93abd7d73862b0593e4a8b403f237a975a9719cc
Ignacio Jonas <ignaciojonas@gmail.com>
2018-11-07 03:35:45 -0300

    Add Tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,19 +3,21 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ProductTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testListProducts()
     {
+        $product = factory(Product::class)->create();
         $response = $this->get('/');
-
+        $response->assertSeeText($product->nombre);
         $response->assertStatus(200);
     }
 }
